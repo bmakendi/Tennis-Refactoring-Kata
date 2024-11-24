a5d1c390267bf65fad6cc70d73f358d90e3a2f86
Bryan Makendi <bryan.makendi@gmail.com>
2024-11-24 02:29:01 +0100

    refactor: introduce class Player to avoid primitiv obsession

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -1,14 +1,28 @@
 import { TennisGame } from "./TennisGame";
 
+class Player {
+  private name: string;
+  private score: number;
+
+  constructor(name: string) {
+    this.name = name;
+    this.score = 0;
+  }
+}
+
 export class TennisGame1 implements TennisGame {
   private m_score1: number = 0;
   private m_score2: number = 0;
   private player1Name: string;
   private player2Name: string;
+  private player1: Player;
+  private player2: Player;
 
   constructor(player1Name: string, player2Name: string) {
     this.player1Name = player1Name;
     this.player2Name = player2Name;
+    this.player1 = new Player(player1Name);
+    this.player2 = new Player(player2Name);
   }
 
   wonPoint(playerName: string): void {
@@ -16,7 +30,7 @@ export class TennisGame1 implements TennisGame {
     else this.m_score2 += 1;
   }
 
-  currentEquality() {
+  displayScoreEquality() {
     switch (this.m_score1) {
       case 0:
         return "Love-All";
@@ -44,7 +58,7 @@ export class TennisGame1 implements TennisGame {
     }
   }
 
-  checkPointDiffAndReturnAdvOrWin() {
+  displayEndgameScore() {
     const minusResult: number = this.m_score1 - this.m_score2;
     if (minusResult === 1) return "Advantage player1";
     if (minusResult === -1) return "Advantage player2";
@@ -54,10 +68,10 @@ export class TennisGame1 implements TennisGame {
 
   getScore(): string {
     if (this.m_score1 === this.m_score2) {
-      return this.currentEquality();
+      return this.displayScoreEquality();
     }
     if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      return this.checkPointDiffAndReturnAdvOrWin();
+      return this.displayEndgameScore();
     }
     return `${this.displayPlayerScore(this.m_score1)}-${this.displayPlayerScore(this.m_score2)}`;
   }
