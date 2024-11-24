67846597e9cc20269e2affae333778dbcc52b261
Bryan Makendi <bryan.makendi@gmail.com>
2024-11-24 13:30:50 +0100

    refactor: replace local variables by introduced class variables

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -8,30 +8,32 @@ class Player {
     this.name = name;
     this.score = 0;
   }
+
+  getScore() {
+    return this.score;
+  }
+
+  didWinPoint(playerName: string) {
+    if (playerName === this.name) this.score += 1;
+  }
 }
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
-  private player1Name: string;
-  private player2Name: string;
   private player1: Player;
   private player2: Player;
 
   constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
     this.player1 = new Player(player1Name);
     this.player2 = new Player(player2Name);
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === this.player1Name) this.m_score1 += 1;
-    else this.m_score2 += 1;
+    this.player1.didWinPoint(playerName);
+    this.player2.didWinPoint(playerName);
   }
 
   displayScoreEquality() {
-    switch (this.m_score1) {
+    switch (this.player1.getScore()) {
       case 0:
         return "Love-All";
       case 1:
@@ -43,8 +45,8 @@ export class TennisGame1 implements TennisGame {
     }
   }
 
-  displayPlayerScore(tempScore: number) {
-    switch (tempScore) {
+  displayPlayerScore(score: number) {
+    switch (score) {
       case 0:
         return "Love";
       case 1:
@@ -59,7 +61,8 @@ export class TennisGame1 implements TennisGame {
   }
 
   displayEndgameScore() {
-    const minusResult: number = this.m_score1 - this.m_score2;
+    const minusResult: number =
+      this.player1.getScore() - this.player2.getScore();
     if (minusResult === 1) return "Advantage player1";
     if (minusResult === -1) return "Advantage player2";
     if (minusResult >= 2) return "Win for player1";
@@ -67,12 +70,12 @@ export class TennisGame1 implements TennisGame {
   }
 
   getScore(): string {
-    if (this.m_score1 === this.m_score2) {
+    if (this.player1.getScore() === this.player2.getScore()) {
       return this.displayScoreEquality();
     }
-    if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+    if (this.player1.getScore() >= 4 || this.player2.getScore() >= 4) {
       return this.displayEndgameScore();
     }
-    return `${this.displayPlayerScore(this.m_score1)}-${this.displayPlayerScore(this.m_score2)}`;
+    return `${this.displayPlayerScore(this.player1.getScore())}-${this.displayPlayerScore(this.player2.getScore())}`;
   }
 }
